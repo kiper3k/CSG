e883b0b6122210cfb4f7264e6104312d8e81cc2c
kiper3k <kiper3k@gmail.com>
2018-01-22 12:27:49 +0100

    edited Rectangle class

--- src/csg/Rectangle.java
+++ src/csg/Rectangle.java
@@ -5,15 +5,73 @@
  */
 package csg;
 
+import java.awt.geom.Rectangle2D;
+
 /**
  *
  * @author kiper
  */
-public class Rectangle implements Shape{
+public class Rectangle extends Rectangle2D.Double{
+    
+    public Rectangle(){
+        super();
+    }
+
+    private Rectangle(double d, double d0, double width, double height) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public boolean contains(double x, double y) {
+        return super.contains(x,y);
+    }
 
     @Override
-    public boolean contains(float x, float y) {
+    public void setRect(double d, double d1, double d2, double d3) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int outcode(double d, double d1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Rectangle2D createIntersection(Rectangle2D rd) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Rectangle2D createUnion(Rectangle2D rd) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getX() {
+        return super.getX();
+    }
+
+    @Override
+    public double getY() {
+        return super.getY();
+    }
+
+    @Override
+    public double getWidth() {
+        return super.getWidth();
+    }
+
+    @Override
+    public double getHeight() {
+        return super.getHeight();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return super.isEmpty();
+    }
+    
+    public Rectangle translate(){
+        return new Rectangle(this.getX()+x, this.getY()+y, this.getWidth(), this.getHeight());
     }
     
 }
