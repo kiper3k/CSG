d02d7ca4dc486dfac506d69b8e11620ac02ecb56
Marta Motyka <32452829+marrtam@users.noreply.github.com>
2018-01-22 03:38:34 +0100

    Create Elipsa.java

---
+++ src/csg/Elipsa.java
@@ -0,0 +1,34 @@
+import static java.lang.Math.pow;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Elipsa extends ShapeBase
+{
+    private float a,b;
+    
+    public Elipsa(float a, float b) {
+        this.a = a;
+        this.b = b;
+    }
+    
+    @Override
+    public boolean contains(float x, float y) {
+        boolean isContains = ( pow(x,2)/pow(a,2) + pow(y,2)/pow(b,2) <= 1 ) ;
+        return isContains;
+    }
+
+    
+    
+    @Override
+    public String toString() 
+    {
+        return "Ellipse(" + this.a + ", " + this.b + ")";
+    }
+
+    @Override
+    public List getShapes() 
+    {
+        List<Shape> shapes = new ArrayList<>();
+        return shapes;
+    }
+}
