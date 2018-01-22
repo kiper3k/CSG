d0619972c99306e90acd6734890e19d8baf0b80f
kiper3k <kiper3k@gmail.com>
2018-01-22 04:07:50 +0100

    convert BoofCV Polygons to java.awt Rectangles

--- src/csg/CSG.java
+++ src/csg/CSG.java
@@ -27,9 +27,11 @@ import boofcv.gui.feature.VisualizeShapes;
 import boofcv.gui.image.ShowImages;
 import boofcv.io.image.ConvertBufferedImage;
 import boofcv.struct.image.GrayU8;
+import georegression.struct.point.Point2D_F64;
 import georegression.struct.shapes.Polygon2D_F64;
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import org.ddogleg.struct.FastQueue;
 
@@ -51,11 +53,11 @@ public class CSG {
         int height;   //height of the image
         
         List<Shape> shapes = new ArrayList<>();
-        shapes.add(new Rectangle2D.Double(3.0, 3.0, 5.0, 2.0));
-        System.out.println(shapes.get(0).contains(0, 0));
-        System.out.println(shapes.get(0).contains(4, 4));
-        shapes.add(new Rectangle2D.Double(2.0, 2.0, 2.0, 4.0));
-        System.out.println(shapes.get(0).intersects((Rectangle2D) shapes.get(1)));
+//        shapes.add(new Rectangle2D.Double(3.0, 3.0, 5.0, 2.0));
+//        System.out.println(shapes.get(0).contains(0, 0));
+//        System.out.println(shapes.get(0).contains(4, 4));
+//        shapes.add(new Rectangle2D.Double(2.0, 2.0, 2.0, 4.0));
+//        System.out.println(shapes.get(0).intersects((Rectangle2D) shapes.get(1)));
         
         BinaryEllipseDetector<GrayU8> ellipseDetector;
         ellipseDetector = FactoryShapeDetector.ellipse(null, GrayU8.class);
@@ -69,6 +71,8 @@ public class CSG {
           f = new File("resources/input.jpg"); //image file path
 //          image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
           image = ImageIO.read(f);
+          width = image.getWidth();
+          height = image.getHeight();
           System.out.println("Reading complete.");
 //          System.out.println(image.getHeight() + " " + image.getWidth());
         }catch(IOException e){
@@ -97,9 +101,9 @@ public class CSG {
             VisualizeShapes.drawPolygon(foundRectangles.get(0), true, g2);
         }
         
-        ListDisplayPanel panel = new ListDisplayPanel();
-        panel.addImage(image,new File("figures").getName());
-        ShowImages.showWindow(panel,"Detected Shapes",true);
+//        ListDisplayPanel panel = new ListDisplayPanel();
+//        panel.addImage(image,new File("figures").getName());
+//        ShowImages.showWindow(panel,"Detected Shapes",true);
         
         width = image.getWidth();
         height = image.getHeight();
@@ -112,6 +116,29 @@ public class CSG {
           System.out.println("Writing complete.");
         }catch(IOException e){
           System.out.println("Error: "+e);
+        }
+        
+        
+        // create java.awt rectangles
+        Point2D_F64 p1, p2, p3, p4;
+        for (int i=0; i<foundRectangles.size(); i++){
+            System.out.println(foundRectangles.get(i).getClass());
+                p1 = foundRectangles.get(i).vertexes.get(0);
+                p2 = foundRectangles.get(i).vertexes.get(1);
+                p3 = foundRectangles.get(i).vertexes.get(2);
+                p4 = foundRectangles.get(i).vertexes.get(3);
+                System.out.println(p1);
+                System.out.println(p2);
+                System.out.println(p3);
+                System.out.println(p4);
+                System.out.println(p1.distance(p2));
+                System.out.println(p2.distance(p3));
+                
+                System.out.println(foundRectangles.get(i).vertexes.get(0).getClass());
+                System.out.println(foundRectangles.get(i).vertexes.get(0).y);
+                System.out.println(foundRectangles.get(i).getSideLength(i));
+                
+                shapes.add(new Rectangle2D.Double(p2.x, p2.y, p2.distance(p1), p2.distance(p3)));
         }
     }
     
